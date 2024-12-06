a51673d5a0770ed83bbaceb8f3b2b786ee1a5cdc
Stella Mirri <stella.mirri@etu.minesparis.psl.eu>
2024-12-06 23:19:56 +0100

    j'ajoute la fonction swap au repo

--- swap.cpp
+++ swap.cpp
@@ -1,23 +1,39 @@
 # include <iostream>
 
-void swap(int * pa, int * pb) {
+//swap avec des pointeurs :
+void swap1(int * pa, int * pb) {
     int t = (*pa);
     *pa = *pb;
     *pb = t;
 }
 
+//swap avec des références : 
+void swap2(int& a, int& b) {      //on rajoute des références en paramètres
+//de la fonction comme ça les valeurs d'origine sont effectivement 
+//changées et c'est pas juste des copies qui sont créées.
+    int t = a;
+    a = b;
+    b = t;
+}
+
 int main() {
     int a = 82;
     int b = -72;
-    int * pa = &a;
-    int * pb = &b;
-    swap(pa,pb);
+    swap2(a, b);
     std:: cout << a << ' ' << b << std::endl;
     return 0;
 }
 
-void swap(int x, int y) {
-    int t = x;
-    x = y;
-    y = t;
-}
+//int main1() {
+//    int a = 82;
+//    int b = -72;
+//    int * pa = &a;
+//    int * pb = &b;
+//    swap1(pa,pb);
+//    std:: cout << a << ' ' << b << std::endl;
+//    return 0;
+//}
+
+
+
+
